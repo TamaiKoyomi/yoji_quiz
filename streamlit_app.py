42afa9122c67a31360291b88ec1cdaa10c2c7ec1
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-11-05 08:04:19 +0000

    afds

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,9 @@ st.title('算出')
 ramuda_ori = st.number_input('とうちゃくりつ',0)
 myu_ori = st.number_input('さーびすりつ',0)
 
-ramuda = 1 / ramuda_ori
-myu = 1 / myu_ori
+if ramuda_ori != 0 and myu_ori != 0:
+    ramuda = 1 / ramuda_ori
+    myu = 1 / myu_ori
 
 ro = ramuda / myu
 
